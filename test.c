92df0085a03913c69c3203c99b4412b0f92db739
Marcello Fabrizio <marcfabrizio10@gmail.com>
2021-09-12 18:47:10 -0300

    Questão 2

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 int main()
 {
       int count = 0;
-      double n = 8;
+      double n = 10;
 
       int inner_j = 0;
       int inner_k = 0;
@@ -13,9 +13,37 @@ int main()
 
       int i, j, k;
 
-      for (j = 1; j <= n; j = j * 2)
-            for (i = j; i > 1; i = i / 2)
-                  count++;
+      // for (j = 1; j <= n; j = j * 2)
+      // for (i = j; i > 1; i = i / 2)
+      // count++;
+
+      for (i = 0; i < n; i++)
+      {
+            for (j = i + 1; j < n; j++)
+            {
+                  for (k = 0; k < n; k++)
+                  {
+                        // Ops. Fundamentais
+                        count++;
+                  }
+            }
+      }
+      // for (i = 0; i < n; i++)
+      // {
+
+      //       for (k = n - 1; k >= i; k--)
+      //       {
+      //             count++;
+      //       }
+
+      //       for (k = i + 1; k < n; k++)
+      //       {
+      //             for (j = n - 1; j >= i; j--)
+      //             {
+      //                   count++;
+      //             }
+      //       }
+      // }
       // printf("Inner i %d\n", inner_i);
       // printf("Inner k %d\n", inner_k);
       // printf("Inner j %d\n", inner_j);
